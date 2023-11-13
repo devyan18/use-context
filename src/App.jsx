05f83a4e1751160aa7b59a2c95e9b36ebeeed808
devyan18 <epache17@gmail.com>
2023-11-13 18:08:11 -0300

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Router from "./Router";
-import Controllers from "./components/Controllers";
 
 function App() {
   return (
     <div className="App">
-      <Controllers />
       <Router />
     </div>
   );
